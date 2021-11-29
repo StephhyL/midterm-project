99de9b613a0321da588267bfdad68b36d4694d3a
StephhyL <Stephanie Li>
2021-11-29 18:52:40 -0500

    altered code for testing insert new row into table

--- db/queries/new_cart_queries.js
+++ db/queries/new_cart_queries.js
@@ -4,15 +4,18 @@ const connection = require('../connection');
 const getNewCart = (object) => {
   const queryString =
   `
-  INSERT INTO carts(user_id, notes)
-  VALUES ($1, $2)
+  INSERT INTO carts(user_id, total_in_cents, notes)
+  VALUES ($1, $2, $3)
   RETURNING *
   `
 
-  const values = [object["user_id"], object.notes]
+  const values = [object["user_id"], object["total_price"], object.notes]
 
   return connection.query(queryString, values)
-    .then(res => console.log(res.rows))
+    .then(res => {
+      console.log(res.rows);
+      return res.rows;
+    })
     .catch((err)=> {
       console.log("OH NO ERROR")
       console.log(err.message)
