4b41ed9719a0e625ef2e36bce351bcd81d68c28c
StephhyL <Stephanie Li>
2021-11-29 18:53:25 -0500

    updated database wwith default time for time_submitted

--- db/schema/03_carts.sql
+++ db/schema/03_carts.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS carts CASCADE;
 CREATE TABLE carts (
   id SERIAL PRIMARY KEY NOT NULL,
   user_id INTEGER REFERENCES users(id) NOT NULL,
-  submitted_time TIMESTAMP NOT NULL,
+  submitted_time TIMESTAMP DEFAULT now(),
   duration_in_seconds SMALLINT DEFAULT 0,
   completed BOOLEAN DEFAULT false,
   total_in_cents INTEGER NOT NULL DEFAULT 0,
