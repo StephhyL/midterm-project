2aa5e7e0c0185eded05c2c23b4b0617e36fab405
StephhyL <Stephanie Li>
2021-11-29 14:20:51 -0500

    trying to work out logic

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -26,6 +26,7 @@ router.get("/", (req, res) => {
 //**but does not redirect to restaurant home because that is only for RESTAURANT OWNER */
 router.post("/", (req, res) => {
 
+  // ADD IF STATEMENT. IF NOTHING IN CART, THEN ALERT. OTHERWISE REDIRECT
   console.log("route reached")
   res.redirect("/message")
 
