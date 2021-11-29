7851a8e749115474a935b2287c082b7f8544ed25
StephhyL <Stephanie Li>
2021-11-29 11:38:20 -0500

    attempted to work out some logic of showing just the newest cart

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -3,13 +3,34 @@ const router = express.Router();
 const restaurantFns = require('../db/queries/restaurant_queries')
 
 router.get("/", (req, res) => {
-  restaurantFns.getRestaurant()//r return either rows or error
+
+  //FEEL LIKE SHOULD BE IN THE POST
+  restaurantFns.getNewestOrder()
     .then((carts) => {
-      // res.json({ food_items });
       const user = null;
-      res.render('restaurant', { carts, user });
+      const templateVars = {carts, user}
+      res.render('restaurant', templateVars)
     })
+
+
+  // restaurantFns.getAllCartItems()
+  //   .then((carts) => {
+  //     const user = null;
+  //     res.render('restaurant', { carts, user });
+  //   })
 });
+
+
+//route is activated when user clicks CHECKOUT, does something, then REDIRECTS TO "restaurant/" aka GET "/" in this file
+router.post("/", (req, res) => {
+
+
+
+
+
+})
+
+
 
 
 
