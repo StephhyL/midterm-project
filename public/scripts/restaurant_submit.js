7494af0ab498afb5a1888edb617a4096b6711abd
StephhyL <Stephanie Li>
2021-12-01 12:38:43 -0500

    commented out everything, not needed at this time

--- public/scripts/restaurant_submit.js
+++ public/scripts/restaurant_submit.js
@@ -1,21 +1,41 @@
-$(()=> {
+// $(()=> {
 
-  const submitBtns = $(".submit-btn");
-  console.log("submitBtns[0]---->", $(submitBtns[0]));
 
-  for (let i=0; i < submitBtns.length; i++) {
-    let button = submitBtns[i];
-    button.addEventListener("click", function() {
-      let estTime = $($(button)).closest(".time-container").find(".estimated-time").val();
+//   const submitTime = (event) => {
+//     let submitBtnClicked = $(event.target);
+//     let estTime = submitBtnClicked.closest(".time-container").find(".estimated-time").val();
+//     let estTimeMessage = `Your order is estimated to be ready in ${estTime} minutes. We will notify you again when the order is actually ready! Stay tuned!`
+//     //hard coded post request
+//     $.post("/login/2", estTimeMessage);
+//   };
+// });
+//   const submitTime = (event) => {
+//     let submitBtnClicked = event.target;
+//     let estTime = submitBtnClicked.closest(".time-container").find(".estimated-time").val();
+//     let estTimeMessage = `Your order is estimated to be ready in ${estTime} minutes. We will notify you again when the order is actually ready! Stay tuned!`
 
-      const estTimeMessage = `Your order is estimated to be ready in ${estTime} minutes. We will notify you again when the order is actually ready! Stay tuned!`
+//     //hard coded post request
+//     $.post("/login/2", estTimeMessage);
+//   }
+// });
 
-      //hard coded post request
-      $.post("/login/2", estTimeMessage);
 
-    })
+  // const submitBtns = $(".submit-btn");
+  // console.log("submitBtns[0]---->", $(submitBtns[0]));
 
-  }
+  // for (let i=0; i < submitBtns.length; i++) {
+  //   let button = submitBtns[i];
+  //   button.addEventListener("click", function() {
+  //     let estTime = $($(button)).closest(".time-container").find(".estimated-time").val();
+
+  //     let estTimeMessage = `Your order is estimated to be ready in ${estTime} minutes. We will notify you again when the order is actually ready! Stay tuned!`
+
+  //     //hard coded post request
+  //     $.post("/login/2", estTimeMessage);
+
+  //   })
+
+  // }
 
 
   // FOR SINGLE UNQUIE BUTTON
@@ -31,4 +51,4 @@ $(()=> {
 
   // })
 
-})
+// })
