6ae6eaff23a21b3a4351fa82346a43d9a9c5e834
StephhyL <Stephanie Li>
2021-12-02 11:20:33 -0500

    changed data to reflect pizzas

--- db/seeds/02_food_items.sql
+++ db/seeds/02_food_items.sql
@@ -1,11 +1,14 @@
 -- food_items table seeds here (Example)
-INSERT INTO food_items (name, description, img_url, price_in_cents) VALUES ('coffee','dark roast no sugar more like an expresso from the middle east', 'img', 5000);
-INSERT INTO food_items (name, description, img_url, price_in_cents) VALUES ('Shawarma', 'Brown color, wiht relish ', 'img2', 10000 );
-INSERT INTO food_items (name, description, img_url, price_in_cents) VALUES ('Pizza', 'Pepperoni oven', 'img3', 9000 );
-INSERT INTO food_items (name, description, img_url, price_in_cents) VALUES ('Hamburger', 'Home made buns', 'img4', 75000 );
-INSERT INTO food_items (name, description, img_url, price_in_cents) VALUES ('Pasta', 'with Parmesan', 'img5', 13000 );
-INSERT INTO food_items (name, description, img_url, price_in_cents) VALUES ('Salad', 'Rainbow salad', 'img6', 12000 );
-INSERT INTO food_items (name, description, img_url, price_in_cents) VALUES ('Chocolate Cake', 'oven made', 'img7', 65000 );
-INSERT INTO food_items (name, description, img_url, price_in_cents) VALUES ('Chicken', 'Brown color with rice', 'img8', 14000 );
-INSERT INTO food_items (name, description, img_url, price_in_cents) VALUES ('Yogurt', 'Strawberry home made style', 'img9', 10000 );
-INSERT INTO food_items (name, description, img_url, price_in_cents) VALUES ('Schnitzel', 'Chicken with bread', 'img10', 20000 );
+INSERT INTO food_items (name, description, img_url, price_in_cents)
+
+VALUES
+('Classic','Our signature basic. Mozzarella cheese paired with our homemade tomato sauce.', 'https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80', 3000),
+('Red Classic', 'Our signature basic topped with smoked pepperoni.', 'https://media.istockphoto.com/photos/hot-homemade-pepperoni-pizza-picture-id521403691?b=1&k=20&m=521403691&s=170667a&w=0&h=CgTHeZO7pZsmL4dgxlpq4AaAGaeBYNA5gRCH9u_3R58=', 3500 ),
+('Golden Pizza', 'Pineapple on our pizza. Yes, pineapples. Give it a try.', 'https://media.istockphoto.com/photos/homemade-pineapple-and-ham-hawaiian-pizza-picture-id537640710?b=1&k=20&m=537640710&s=170667a&w=0&h=iJs3ajTyZpg2KZqId7meMQ6j8GQZVC935DDCbUw0gbE=', 4000 ),
+('Mushroom Pizza', 'Mushrooms on top of our signature base.', 'https://media.istockphoto.com/photos/gourmet-homemade-mushroom-pizza-picture-id864746232?k=20&m=864746232&s=612x612&w=0&h=PLMe9ByymLIcqJKkEx_RQ0PYh7tlJ2eFUBOrRLVq0vg=', 4000 ),
+('Tropical Mix Pizza', 'Colourful spread of peppers, mushrooms, and onions on top of our classic red pizza.', 'https://media.istockphoto.com/photos/homemade-hot-supreme-pizza-picture-id523441817?k=20&m=523441817&s=612x612&w=0&h=QwgfnWzm33d88eYh1uTjv66BMxdmz7xd0ObQk12ZFmU=', 4500),
+('Chicken Pizza', 'Chicken and onions with our barbeque base.', 'https://media.istockphoto.com/photos/homemade-barbecue-chicken-pizza-picture-id484538935?k=20&m=484538935&s=612x612&w=0&h=9FmADEMzlBBlzLYW3MUAdC27mjZ9xjUU-XAm_GL0C5M=', 4500 ),
+('Pizza Bites', 'Our pizzas made miniature. Choose any of the above pizzas.', 'https://media.istockphoto.com/photos/sausage-and-pepper-pizza-bagels-picture-id513585902?k=20&m=513585902&s=612x612&w=0&h=X0Qx8kCJTZ6DivlBVOfok47FJ0n8CUE2k-KTWFsCSxA=', 2500 ),
+('Mystery Pizza', 'For those days that you are feeling adventurous. Let us decide what to make for you. Each Mystery Pizza is unique.', 'https://images.unsplash.com/photo-1561350111-7daa4f284bc6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80', 20000 ),
+('Pizza Salad', 'Not actually a pizza - it is our house salad that pairs well with any of our pizzas!', 'https://images.unsplash.com/photo-1569760142069-bc6838de16c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTl8fHNhbGFkfGVufDB8MHwwfHw%3D&auto=format&fit=crop&w=500&q=60', 12000 ),
+('Surprise Drink', 'Mystery Drink that we think will pair well with your selected pizza(s)!', 'https://images.unsplash.com/photo-1601477575132-df687f64f14c?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80', 5000 );
