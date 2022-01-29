b8336d348e3e4a70da5ea7b01bb8c524439d3e7a
Stephanie Li <stephanieli@Stephanies-MacBook-Air.local>
2022-01-28 22:35:47 -0500

    something

--- routes/login.js
+++ routes/login.js
@@ -1,107 +1,100 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const userFns = require('../db/queries/users_queries');
-const homeFns = require('../db/queries/home_queries');
-const helperFns = require('../public/scripts/helper');
-const restaurantFns = require('../db/queries/restaurant_queries');
-const socket_client = require('../socket-client');
-const twilio = require('twilio'); //Twilio sms api
-const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const userFns = require("../db/queries/users_queries");
+const homeFns = require("../db/queries/home_queries");
+const helperFns = require("../public/scripts/helper");
+const restaurantFns = require("../db/queries/restaurant_queries");
+const socket_client = require("../socket-client");
+const twilio = require("twilio"); //Twilio sms api
+const client = new twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 
-
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const user = req.body;
-  return userFns.getUserByName(user.user)
+  return userFns
+    .getUserByName(user.user)
     .then((data) => {
       if (helperFns.passwordCheck(user.psw, data)) {
-        return res.redirect(`/login/${data.rows[0].id}`)
+        return res.redirect(`/login/${data.rows[0].id}`);
       }
       res.status(401);
     })
-    .catch(err => res.status(401).send(err))
-})
-
-router.get("/:id", (req, res) => {
-  userFns.getUserById(req.params.id)
-    .then((user) => {
-      if (!helperFns.adminOrCustomer(user.id)) {
-        homeFns.getHome()
-          .then((food_items) => {
-            return res.render('index', { user, food_items });
-          })
-      } else {
-        restaurantFns.getNewestOrder()
-          .then((carts) => {
-            const templateVars = { carts, user }
-            return res.render('restaurant', templateVars)
-          })
-      }
-    })
+    .catch((err) => res.status(401).send(err));
 });
 
-router.post('/completed/:id', (req, res) => {
-  const response = req.body;
-  userFns.completedOrder(response)
-    .then(data => {
-      socket_client()
-        .then((socket) => {
-          socket.emit("orderCompleted", response)
+router.get("/:id", (req, res) => {
+  userFns.getUserById(req.params.id).then((user) => {
+    if (!helperFns.adminOrCustomer(user.id)) {
+      homeFns.getHome().then((food_items) => {
+        return res.render("index", { user, food_items });
+      });
+    } else {
+      restaurantFns.getNewestOrder().then((carts) => {
+        const templateVars = { carts, user };
+        return res.render("restaurant", templateVars);
+      });
+    }
+  });
+});
 
-          const customerComplete = `Your order #${response.cartId} is ready for pickup. See you soon!`
-          return client.messages
-            .create({
-              body: customerComplete,
-              to: process.env.TWILIO_CUSTOMER_PHONE_NUMBER, // Text this number
-              from: process.env.TWILIO_PHONE_NUMBER, // From a valid Twilio number
-            })
-            .then((message) => console.log(message.sid))
-            .catch(err => console.log(err));
-        })
+router.post("/completed/:id", (req, res) => {
+  const response = req.body;
+  userFns
+    .completedOrder(response)
+    .then((data) => {
+      socket_client().then((socket) => {
+        socket.emit("orderCompleted", response);
+
+        // const customerComplete = `Your order #${response.cartId} is ready for pickup. See you soon!`
+        // return client.messages
+        //   .create({
+        //     body: customerComplete,
+        //     to: process.env.TWILIO_CUSTOMER_PHONE_NUMBER, // Text this number
+        //     from: process.env.TWILIO_PHONE_NUMBER, // From a valid Twilio number
+        //   })
+        //   .then((message) => console.log(message.sid))
+        //   .catch(err => console.log(err));
+      });
     })
     .catch((err) => {
-      console.log("yeah, an error :(")
-      console.log(err.message)
-    })
+      console.log("yeah, an error :(");
+      console.log(err.message);
+    });
 });
 
 router.post("/:id", (req, res) => {
-  const response = { time: helperFns.minutesFromRestaurant(req.body.time), cartId: req.body.cartId };
+  const response = {
+    time: helperFns.minutesFromRestaurant(req.body.time),
+    cartId: req.body.cartId,
+  };
   const timeObj = {
     cartId: req.body.cartId,
-    time: req.body.time
+    time: req.body.time,
   };
 
-  userFns.updateTime(response)
-    .then(data => {
-      socket_client()
-        .then((socket) => {
-          const customerUpdate = `Your order #${timeObj.cartId} is being processed. The estimated time for pick up is in ${timeObj.time} minutes. We will notify you again once the food is ready! See you soon!`
+  userFns
+    .updateTime(response)
+    .then((data) => {
+      socket_client().then((socket) => {
+        const customerUpdate = `Your order #${timeObj.cartId} is being processed. The estimated time for pick up is in ${timeObj.time} minutes. We will notify you again once the food is ready! See you soon!`;
 
-
-          socket.emit("time-message", timeObj)
-          return client.messages
-            .create({
-              body: customerUpdate,
-              to: process.env.TWILIO_CUSTOMER_PHONE_NUMBER,
-              from: process.env.TWILIO_PHONE_NUMBER,
-            })
-            .then((message) => console.log(message.sid))
-            .catch(err => console.log(err));
-        })
-    }).catch((err) => {
-      console.log("yeah, an error :(")
-      console.log(err.message)
+        socket.emit("time-message", timeObj);
+        return client.messages
+          .create({
+            body: customerUpdate,
+            to: process.env.TWILIO_CUSTOMER_PHONE_NUMBER,
+            from: process.env.TWILIO_PHONE_NUMBER,
+          })
+          .then((message) => console.log(message.sid))
+          .catch((err) => console.log(err));
+      });
     })
-
-})
-
+    .catch((err) => {
+      console.log("yeah, an error :(");
+      console.log(err.message);
+    });
+});
 
 module.exports = router;
-
-
-
-
-
-
-
-
--- routes/newCart.js
+++ routes/newCart.js
@@ -1,95 +1,84 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const newCartFns = require('../db/queries/new_cart_queries')
-const twilio = require('twilio'); //Twilio sms api
-const client = new twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const newCartFns = require("../db/queries/new_cart_queries");
+const twilio = require("twilio"); //Twilio sms api
+const client = new twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 
-const socket_client = require('../socket-client');
-
+const socket_client = require("../socket-client");
 
 const retrieveFoods = (obj) => {
-  let foods = '';
+  let foods = "";
   for (let food_item in obj) {
     foods += `-- ${obj[food_item].qty} x ${food_item} `;
   }
   return foods;
 };
 
-
 router.post("/", (req, res) => {
   let temp = req.body;
 
-  newCartFns.ultimateFoodInsert(temp).then(resp => {
-    return newCartFns.getCartById(resp.cart_id)
-
-  })
+  newCartFns
+    .ultimateFoodInsert(temp)
+    .then((resp) => {
+      return newCartFns.getCartById(resp.cart_id);
+    })
     .then((data) => {
       let customerMessage = `
       Thank you for placing an order with our restaurant.
       Your confirmation number is ${data.id}.
       Your order includes the following items:
       ${retrieveFoods(temp.addCartFoods)}
-      Your total is: $${(data.total_in_cents) / 100}.
+      Your total is: $${data.total_in_cents / 100}.
       Your notes are: ${data.notes}.
       `;
       let restaurantMessage = `
-      An order was placed by customer id: ${req.body.addCart['user_id']}.
+      An order was placed by customer id: ${req.body.addCart["user_id"]}.
       The order number is ${data.id}.
       The order includes the following items:
       ${retrieveFoods(req.body.addCartFoods)}
-      The total is: $${(data.total_in_cents) / 100}.
+      The total is: $${data.total_in_cents / 100}.
       Customers Notes: ${data.notes}.
       `;
       const orderObj = {
         cartId: data.id,
-        customerId: temp.addCart['user_id'],
+        customerId: temp.addCart["user_id"],
         time: data.submitted_time,
         listOfFoods: retrieveFoods(temp.addCartFoods),
         notes: data.notes,
-        total: (data.total_in_cents) / 100
-      }
+        total: data.total_in_cents / 100,
+      };
 
       socket_client()
         .then((socket) => {
-          socket.emit("inputValue", orderObj)
+          socket.emit("inputValue", orderObj);
         })
         .catch((err) => {
-          console.log("yeah, an error :(")
-          console.log(err.message)
-        })
+          console.log("yeah, an error :(");
+          console.log(err.message);
+        });
 
-      client.messages
-        .create({
-          body: customerMessage,
-          to: process.env.TWILIO_CUSTOMER_PHONE_NUMBER,
-          from: process.env.TWILIO_PHONE_NUMBER,
-        })
-        .then((message) => console.log(message.sid))
-        .catch(err => console.log(err));
-      client.messages
-        .create({
-          body: restaurantMessage,
-          to: process.env.TWILIO_RESTAURANT_PHONE_NUMBER,
-          from: process.env.TWILIO_PHONE_NUMBER,
-        })
-        .then((message) => console.log(message.sid))
-        .catch(err => console.log(err));
+      // client.messages
+      //   .create({
+      //     body: customerMessage,
+      //     to: process.env.TWILIO_CUSTOMER_PHONE_NUMBER,
+      //     from: process.env.TWILIO_PHONE_NUMBER,
+      //   })
+      //   .then((message) => console.log(message.sid))
+      //   .catch((err) => console.log(err));
+      // client.messages
+      //   .create({
+      //     body: restaurantMessage,
+      //     to: process.env.TWILIO_RESTAURANT_PHONE_NUMBER,
+      //     from: process.env.TWILIO_PHONE_NUMBER,
+      //   })
+      //   .then((message) => console.log(message.sid))
+      //   .catch(err => console.log(err));
       res.send(null);
     })
-    .catch(err => console.log(err.message))
+    .catch((err) => console.log(err.message));
 });
 
-
 module.exports = router;
-
-
-
-
-
-
-
-
-
-
-
-
